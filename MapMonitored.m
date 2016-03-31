d892d6c9243a46a44e334da012e04fa516ccedc4
Paco Jain <pacojain@gmail.com>
2016-03-31 11:13:17 -0500

    Added function DoMonitored

--- MapMonitored.m
+++ MapMonitored.m
@@ -5,6 +5,7 @@ BeginPackage["MapMonitored`"]
 
 MapMonitored::usage = "MapMonitored[f, expr] works like Map, but displays a dynamically updated index of the element currently being operated upon."
 CheckMapMonitored::usage = "CheckMapMonitored[f, expr] works like MapMonitored, but additionally exits the Map operation (or performs another action, as specified by the option \"MessageFunction\", on Sequence[index, item value]) whenever a message is generated."
+DoMonitored::usage = "DoMonitored[f, expr] works like Do, but displays a dynamically updated index of the element currently being operated upon."
 Begin["`Private`"]
 
 
@@ -26,6 +27,23 @@ CheckMapMonitored[f_, list_, opts: OptionsPattern[{"DisplayFunction" -> Automati
 	messFunc = OptionValue["MessageFunction"] /. Automatic -> Function[{index, item}, Print[Column[{index, dispFunc[item]}]]; Return[item, Module]];
 	g= (i= First@#2; Check[f[#1], messFunc[First[#2], #1]])&;
 	Block[{i=2}, Monitor[MapIndexed[g, list], i]]
+]
+
+ClearAll[DoMonitored]
+SetAttributes[DoMonitored, HoldAll]
+DoMonitored[body_, iter_, opts: OptionsPattern[{"DisplayFunction" -> None}]]:= Module[
+	{count = 0, dispFunc},
+	dispFunc= Switch[OptionValue["DisplayFunction"], None, Nothing&, _, OptionValue["DisplayFunction"]];
+	Monitor[
+		Do[
+			count++;
+			body,
+			iter
+		], Switch[ OptionValue["DisplayFunction"], 
+			None, count, 
+			_, Column[{count, dispFunc[iter[[1]]]}]
+		]
+	]
 ]
 
 
